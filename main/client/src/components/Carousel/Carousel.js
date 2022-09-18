b6e60ab021b0a783804c85879a4ab1833571ebd4
KingMo <macv123321@gmail.com>
2022-09-17 21:01:23 -0400

    started main body

--- main/client/src/components/Carousel/Carousel.js
+++ main/client/src/components/Carousel/Carousel.js
@@ -1,14 +1,12 @@
 import { Grid, Button } from '@mui/material'
 import React from 'react'
-import "./carousel.css"
-
 
 const Carousel = () => {
   return (
     <Grid>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button> 
+    <Button variant="text">Text</Button>
+    <Button variant="contained">Contained</Button>
+    <Button variant="outlined">Outlined</Button> 
     </Grid>
       
   )
