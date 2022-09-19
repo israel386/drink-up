a8a60eef8c5e37b251fb7b94909006a6aef76c5e
KingMo <macv123321@gmail.com>
2022-09-19 15:56:21 -0400

    added pictures to cards and fixed layout of cards

--- main/client/src/components/Card/Card.js
+++ main/client/src/components/Card/Card.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { Grid, Button, Card, CardContent, CardActions, Typography, CardMedia } from '@mui/material'
 import { Container } from '@mui/system';
+import Checkbox from '@mui/material/Checkbox';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import Favorite from '@mui/icons-material/Favorite';
+
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 
-const drinks = ["Pina Colada","Margarita", "Old Fashion"];
+const drinks = ["Mexican Mule","Margarita", "Old Fashion"];
 
 
 // const cardLayout = () => {
@@ -11,7 +16,7 @@ const drinks = ["Pina Colada","Margarita", "Old Fashion"];
 //       <Card sx={{ maxWidth: 345 }}>
 //         <CardMedia
 //         component="img"
-//         height="140"
+//         height="200"
 //         image="/static/images/cards/contemplative-reptile.jpg"
 //         alt="green iguana"
 //         />
@@ -35,29 +40,129 @@ const drinks = ["Pina Colada","Margarita", "Old Fashion"];
 
 const DrinkCard = () => {
   return (
-    <Container>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-        component="img"
-        height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
-        alt="green iguana"
-        />
-        <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {drinks}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-        </Typography>
-        </CardContent>
-        <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
-    </Container>
+    <Grid container spacing={2} justifyContent="center" alignItems="center">
+      <Grid item>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+          component="img"
+          height="200"
+          image="./images/tequila_images/mexicanMule.png"
+          alt="mexicanMule"
+          />
+          <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {drinks[0]}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over 6,000
+              species, ranging across all continents except Antarctica
+          </Typography>
+          </CardContent>
+          <CardActions>
+            <Checkbox {...label} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </Grid>
+
+      <Grid item>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+          component="img"
+          height="200"
+          image="./images/tequila_images/bloodyMaria.png"
+          alt="Bloody Maria"
+          />
+          <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {drinks[0]}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over 6,000
+              species, ranging across all continents except Antarctica
+          </Typography>
+          </CardContent>
+          <CardActions>
+            <Checkbox {...label} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </Grid>
+
+      <Grid item>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+          component="img"
+          height="200"
+          image="./images/tequila_images/Paloma.png"
+          alt="Paloma"
+          />
+          <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {drinks[0]}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over 6,000
+              species, ranging across all continents except Antarctica
+          </Typography>
+          </CardContent>
+          <CardActions>
+            <Checkbox {...label} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </Grid>
+
+      <Grid item>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+          component="img"
+          height="200"
+          image="./images/tequila_images/TequilaSour.png"
+          alt="Tequila Sour"
+          />
+          <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {drinks[0]}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over 6,000
+              species, ranging across all continents except Antarctica
+          </Typography>
+          </CardContent>
+          <CardActions>
+            <Checkbox {...label} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </Grid>
+
+      <Grid item>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+          component="img"
+          height="200"
+          image="./images/tequila_images/TequilaSunrise.png"
+          alt="Tequila Sunrise"
+          />
+          <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {drinks[0]}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over 6,000
+              species, ranging across all continents except Antarctica
+          </Typography>
+          </CardContent>
+          <CardActions>
+            <Checkbox {...label} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </Grid>
+
+      
+    </Grid>
     
   );
 }
