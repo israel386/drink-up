72929a3803adc2b526e2b90d63688ddc270721c6
KingMo <macv123321@gmail.com>
2022-09-18 22:39:56 -0400

    updated code

--- main/client/src/components/Card/Card.js
+++ main/client/src/components/Card/Card.js
@@ -9,33 +9,34 @@ const drinks = ["Pina Colada","Margarita", "Old Fashion"];
 // const cardLayout = () => {
 //     return (
 //       <Card sx={{ maxWidth: 345 }}>
-//           <CardMedia
-//           component="img"
-//           height="140"
-//           image="/static/images/cards/contemplative-reptile.jpg"
-//           alt="green iguana"
-//           />
-//           <CardContent>
-//           <Typography gutterBottom variant="h5" component="div">
-//               Pina Colada
-//           </Typography>
-//           <Typography variant="body2" color="text.secondary">
-//               abcdefghijklmnopqrstuvwxyz
-//           </Typography>
-//           </CardContent>
-//           <CardActions>
-//           <Button size="small">Learn More</Button>
-//           </CardActions>
-//       </Card>
-      
+//         <CardMedia
+//         component="img"
+//         height="140"
+//         image="/static/images/cards/contemplative-reptile.jpg"
+//         alt="green iguana"
+//         />
+//         <CardContent>
+//         <Typography gutterBottom variant="h5" component="div">
+//             Pina Colada
+//         </Typography>
+//         <Typography variant="body2" color="text.secondary">
+//             abcdefghijklmnopqrstuvwxyz
+//         </Typography>
+//         </CardContent>
+//         <CardActions>
+//         <Button size="small">Learn More</Button>
+//         </CardActions>
+//       </Card> 
 //     );
 // }
 
 
 
+
 const DrinkCard = () => {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Container>
+      <Card sx={{ maxWidth: 345 }}>
         <CardMedia
         component="img"
         height="140"
@@ -44,7 +45,7 @@ const DrinkCard = () => {
         />
         <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-            {drinks}
+          {drinks}
         </Typography>
         <Typography variant="body2" color="text.secondary">
             Lizards are a widespread group of squamate reptiles, with over 6,000
@@ -55,7 +56,8 @@ const DrinkCard = () => {
         <Button size="small">Share</Button>
         <Button size="small">Learn More</Button>
         </CardActions>
-    </Card>
+      </Card>
+    </Container>
     
   );
 }
