529153287a2f16a7b05b73600c06c683ae85e901
KingMo <macv123321@gmail.com>
2022-09-19 23:40:42 -0400

    fixed issue

--- main/client/src/components/Categories/Categories.js
+++ main/client/src/components/Categories/Categories.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import {Grid, Button} from '@mui/material'
+// import React from 'react'
+// import {Grid, Button} from '@mui/material'
 
 
-const categories = ["Rum", "Tequila", "Vodka",];
+// const categories = ["Rum", "Tequila", "Vodka",];
 
-const Categories = () => {
-  return (
-    <Grid container direction="row" justifyContent="center" alignItems="center">
-            {/* ADDS WHATEVER YOU ADD INTO "CATERGORIES" ARRAY */}
-        {categories.map((category) => (
-        <Button variant="contained" color="secondaryVar" sx={{m:2}}>{category}</Button>
-        ))}
-    </Grid>
-  )
-}   
+// const Categories = () => {
+//   return (
+//     <Grid container direction="row" justifyContent="center" alignItems="center">
+//             {/* ADDS WHATEVER YOU ADD INTO "CATERGORIES" ARRAY */}
+//         {categories.map((category) => (
+//         <Button variant="contained" color="secondaryVar" sx={{m:2}}>{category}</Button>
+//         ))}
+//     </Grid>
+//   )
+// }   
 
-export default Categories
+// export default Categories
--- main/client/src/pages/Home.js
+++ main/client/src/pages/Home.js
@@ -1,24 +1,20 @@
 import React from "react";
-// import ProductList from "../components/ProductList";
-// import CategoryMenu from "../components/CategoryMenu";
-// import Cart from "../components/Cart";
-import Nav from "../components/NavBar/Nav"
-import Categories from "../components/Categories/Categories";
-import DrinkCard from "../components/Card/Card"
+import ProductList from "../components/ProductList";
+import CategoryMenu from "../components/CategoryMenu";
+import Cart from "../components/Cart";
+// import Nav from "../components/NavBar/Nav"
+// import Categories from "../components/Categories/Categories";
+// import DrinkCard from "../components/Card/Card"
 // import ProductItem from "../components";
 
 
 const Home = () => {
   return (
-    <div>
-      <Nav/>
-      <br/>
-      <Categories/>
-      <br/>
-      {/* <ProductItem/> */}
-     <DrinkCard/>
+    <div className="container">
+      <CategoryMenu/>
+      <ProductList/>
+      <Cart/>
     </div>
-    
   );
 };
 
